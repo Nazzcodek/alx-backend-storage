558c80b18057f0216f074645e528834cf8621eae
Nazzcodek <bellnas09@gmail.com>
2024-01-23 03:02:32 -0800

    11 checked

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,6 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     """this method search school collection with a given topic"""
-    search = [mongo_collection.find({"name": topic})]
+    search = [mongo_collection.find({"topics": topic})]
 
     return search
