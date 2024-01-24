4e6626bcdf5740833538642dbacea4c2ff6334f9
Nazzcodek <bellnas09@gmail.com>
2024-01-24 14:42:08 -0800

    callback

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,56 @@
 """this is a redis catche module"""
 from uuid import uuid4
 import redis
-from typing import Union, Callable
+from typing import Union, Optional, Callable
+
+
+def count_calls(method: Callable) -> Callable:
+    """this is a count method"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """this method wrap the count decorator"""
+        key = method.__qualname__
+        self.redis.incr(key)
+
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """this method store the history of redis call"""
+    key = method.__qualname__
+    key_inputs = key + ":inputs"
+    key_outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ this method wrap the history decorator"""
+        self._redis.rpush(key_inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(key_outputs, str(data))
+        return data
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """this method replay the redis history"""
+    name = method.__qualname__
+
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+
+    print(f"{name} was called {calls} times:")
+
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+
+    for i, o in zip(inputs, outputs):
+        _input = i.decode("utf-8")
+        _output = o.decode("utf-8")
+        print(f"{name}(*{_input}) -> {_output}")
 
 
 class Cache:
